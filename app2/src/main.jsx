e7dcaf82029430a7740926b682decaa164685a9d
Priyanshu <priyanshu41619@gmail.com>
2025-09-11 23:16:29 +0530

    Update main.jsx

--- app2/src/main.jsx
+++ app2/src/main.jsx
@@ -1,17 +1,34 @@
 
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import {Canvas} from ""
+import {Canvas} from "@react-three/fiber"
+import Scene from './Scene'
+import Custom from './Custom'
+const createdCanvasHandler = (state)=>{
+    state.gl.setClearColor("yellow" , 0.6);
+  };
 
 createRoot(document.getElementById('root')).render(
+
 <>
-  <Canvas camera = {{
+  <Canvas gl={{
+    antialias:true,
+    alpha:true,
+  }}
+  // orthographic
+  camera = {{
+
     fov:100,
     near:0.1,
     far:100,
+    // zoom:120,
     position:[2,2,5],
-  }}>
-
+  }}
+  onCreated={createdCanvasHandler}
+  >
+    
+    <Scene/>
+    {/* <Custom/> */}
   </Canvas>
 </>
 )
