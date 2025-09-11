b8457fc866ea2e964256aa3414a6db059129f964
Priyanshu <priyanshu41619@gmail.com>
2025-09-11 22:08:46 +0530

    Create main.jsx

---
+++ app2/src/main.jsx
@@ -0,0 +1,17 @@
+
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import {Canvas} from ""
+
+createRoot(document.getElementById('root')).render(
+<>
+  <Canvas camera = {{
+    fov:100,
+    near:0.1,
+    far:100,
+    position:[2,2,5],
+  }}>
+
+  </Canvas>
+</>
+)
