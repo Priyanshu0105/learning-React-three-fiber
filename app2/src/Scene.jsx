d612a0a86d56a20c72b771cbfc632368e7ffca33
Priyanshu <priyanshu41619@gmail.com>
2025-09-11 22:34:22 +0530

    Update Scene.jsx

---
+++ app2/src/Scene.jsx
@@ -0,0 +1,25 @@
+import { useFrame ,extend , useThree} from "@react-three/fiber";
+import { useRef } from "react";
+import * as THREE from "three"
+import { OrbitControls} from "@react-three/drei";
+
+
+
+const Scene = () =>{
+    const boxRef = useRef();
+    useFrame((state,delta)=>{
+        // boxRef.current.rotation.x +=delta;
+        state.camera.position.x = Math.sin(state.clock.elapsedTime)
+    });
+    return(
+    <>
+
+    {/* <OrbitControls/> */}
+    <mesh ref={boxRef} >
+        <boxGeometry/>
+        <meshNormalMaterial side ={THREE.DoubleSide} />
+    </mesh>
+    </>)
+}
+
+export default Scene;
