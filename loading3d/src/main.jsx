53b550e9a365fc4d94599fa5a2b002d9dcc3dd60
Priyanshu <priyanshu41619@gmail.com>
2025-09-15 02:12:24 +0530

    Initial commit

---
+++ loading3d/src/main.jsx
@@ -0,0 +1,16 @@
+
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import {Canvas} from "@react-three/fiber"
+import Scene from "./Scene"
+
+
+createRoot(document.getElementById('root')).render(
+  <Canvas camera={{
+    fov:100,
+    near:0.1,
+    far:100,
+  }}>
+    <Scene/>
+  </Canvas>
+)
