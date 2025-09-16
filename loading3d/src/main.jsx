0e50aa1c4003c570fc510bc0416c22d676610b04
Priyanshu <priyanshu41619@gmail.com>
2025-09-17 01:05:29 +0530

    Update main.jsx

--- loading3d/src/main.jsx
+++ loading3d/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import {Canvas} from "@react-three/fiber"
 import Scene from "./Scene"
-
+import { Perf } from "r3f-perf"
 
 createRoot(document.getElementById('root')).render(
   <Canvas camera={{
@@ -12,5 +12,6 @@ createRoot(document.getElementById('root')).render(
     far:100,
   }}>
     <Scene/>
+    <Perf position='top-left'/>
   </Canvas>
 )
