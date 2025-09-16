9097d1518dbc4258768ca030e25553850219caf4
Priyanshu <priyanshu41619@gmail.com>
2025-09-17 01:05:31 +0530

    Update Model.jsx

--- loading3d/src/Model.jsx
+++ loading3d/src/Model.jsx
@@ -1,9 +1,20 @@
-import {GLTFLoader} from "three/examples/jsm/loaders/GLTFLoader.js"
+// import {GLTFLoader} from "three/examples/jsm/loaders/GLTFLoader.js"
 import * as THREE from "three"
-import { useLoader } from "@react-three/fiber"
+// import { useLoader } from "@react-three/fiber"
+import { useAnimations, useGLTF } from "@react-three/drei"
+import { useEffect } from "react"
 
 const Model = () => {
-    const model = useLoader(GLTFLoader,"/model/scene.gltf")
+    const model = useGLTF("/model/character.gltf")
+    const animation = useAnimations(model.animations,model.scene)
+    console.log(model);
+    useEffect(() =>{
+        animation.actions.course_loup.play();
+        animation.actions.course_cavalier.play();
+    },[])
     return <primitive position-y={-0.4} object={model.scene} />
+
+
 }
+useGLTF.preload("/model/character.gltf")
 export default Model;
